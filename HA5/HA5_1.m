e4d8049cedc7f91406d3218ba8f8ba4cf5101957
yuxuanx <xiayuxuan1993@outlook.com>
2016-10-07 15:10:01 +0200

    minor changes

--- HA5/HA5_1.m
+++ HA5/HA5_1.m
@@ -14,7 +14,7 @@ xlabel('x')
 legend('h(x)','y')
 
 p_x = normpdf(x,mu_x,sigma_x);
-p_y = normpdf(0.01*x.^3,3.5,sqrt(2));
+p_y = normpdf(h_x,3.5,sqrt(2));
 figure
 hold on
 plot(x,p_x)
@@ -33,7 +33,7 @@ pyx = normpdf(0.01*samples.^3,3.5,sqrt(2));
 w_tilde = 1/n*pyx;
 w = w_tilde/(sum(w_tilde));
 E_xy = sum(samples.*w);
-Var_xy = sum(samples.^2.*w) - E_xy^2;
+Var_xy = mean((E_xy-samples).^2);
 
 % figure
 % hold on
@@ -45,16 +45,13 @@ Var_xy = sum(samples.^2.*w) - E_xy^2;
 %% Task c
 % Resampling
 u = sort(rand(n,1));
-px = normpdf(samples,mu_x,sigma_x);
-px_norm = px/sum(px);
-px_cum = cumsum(px_norm);
+px_cum = cumsum(w);
 [~,idx1] = sort([u;px_cum]);
 idx2 = find(idx1<=n);
 idx = idx2 - (0:n-1)';
 resamples = samples(idx);
 w_resample = ones(n,1)/n;
 E_resample = sum(resamples.*w_resample);
-
 
 p = find([n;diff(idx);n]);
 values = idx(p(1:end-1));
@@ -72,7 +69,9 @@ end
 for i = 1:n_unique
     plot([resamples(i) resamples(i)],[0, w_resample(i)],'r');
 end
-
+xlabel('samples')
+ylabel('p')
+title('Original and new samples after resampling along with their weights')
 
 
 
