1b9ee40993497fa9ff9fb9d05e35f692217ff086
yuxuanx <xiayuxuan1993@outlook.com>
2016-10-08 13:07:03 +0200

    minor changes

--- HA5/HA5_2.m
+++ HA5/HA5_2.m
@@ -3,7 +3,8 @@ clc;clear
 % Parameters
 Q = 1.5;
 R = 2.5;
-T = 1000; % time step
+% The first time step is x_0
+T = 1000+1; % time step
 A = 1;
 H = 1;
 
@@ -31,9 +32,9 @@ end
 % Kalman filtering
 for i = 2:T
     x_hat(i) = A*x_hat(i-1);
-    P(i) = A*P(i-1)*A + Q;
+    P(i) = A*P(i-1)*A' + Q;
     
-    S = H*P(i)*H + R;
+    S = H*P(i)*H' + R;
     K = P(i)*H/S;
     v = y(i) - H*x_hat(i);
     
@@ -58,31 +59,34 @@ xlabel('time step k')
 ylabel('variance')
 title('Variance of state over time')
 
+
 %% Partical filter without resampling
 % Draw samples from proposal (motion model)
 N = 500;
-x_hat = zeros(T,1);
+x_sis = zeros(T,1);
 P_hat = zeros(T,1);
-x_hat(1) = 2;
+x_sis(1) = 2;
 P_hat(1) = 6;
 q = sqrt(Q);
 r = sqrt(R);
-w = 1/N*ones(N,1);
+w = zeros(N,T);
+w(:,1) = 1/N*ones(N,1);
+samples = zeros(N,T);
 for i = 2:T
-    samples = normrnd(x_hat(i-1),q,N,1);
-    pyx = normpdf(samples,y(i),r);
-    w_tilde = w.*pyx;
-    w = w_tilde/sum(w_tilde);
-    x_hat(i) = sum(w.*samples);
-    P_hat(i) = mean((x_hat(i)-samples).^2);
+    samples(:,i-1) = normrnd(x_sis(i-1),q,N,1);
+    pyx = normpdf(samples(:,i-1),y(i),r);
+    w_tilde = w(:,i-1).*pyx;
+    w(:,i) = w_tilde/sum(w_tilde);
+    x_sis(i) = sum(w(:,i).*samples(:,i-1));
+    P_hat(i) = mean((x_sis(i)-samples(:,i-1)).^2);
 end
 
-mse_pf = mean((x-x_hat).^2);
+mse_pf = mean((x-x_sis).^2);
 
 figure
 subplot(2,1,1)
 hold on
-plot(x_hat)
+plot(x_sis)
 plot(x)
 xlabel('time step k')
 ylabel('mean value')
@@ -93,6 +97,8 @@ plot(P_hat)
 xlabel('time step k')
 ylabel('variance')
 title('Variance of state over time')
+
+PostPlot(samples(:,1:end-1),w(:,2:end),x_hat(2:end)',P(2:end)',N,T-1,0)
 
 %% Partical filter with resampling
 for i = 2:T
